686e8252c51269e0647105aec594b1dd538bdd20
phong.vh <phong.vh@kiotviet.com>
2023-10-12 14:09:15 +0700

    #feature: add robots.txt

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  productionBrowserSourceMaps: true,
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
